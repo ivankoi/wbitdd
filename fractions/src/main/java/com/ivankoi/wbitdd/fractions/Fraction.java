779b821dcc6b677b5f3cf651852bad79a039593a
ivankoi <ivankoi@gmail.com>
2017-01-16 23:13:01 +0100

    We removed the integerValue field

--- fractions/src/main/java/com/ivankoi/wbitdd/fractions/Fraction.java
+++ fractions/src/main/java/com/ivankoi/wbitdd/fractions/Fraction.java
@@ -1,18 +1,15 @@
 package com.ivankoi.wbitdd.fractions;
 
 public class Fraction {
-    private final int integerValue;
     private int numerator;
     private int denominator;
 
     public Fraction(int integerValue) {
-        this.integerValue = integerValue;
         this.numerator = integerValue;
         this.denominator = 1;
     }
 
     public Fraction(int numerator, int denominator) {
-        this.integerValue = numerator;
         this.numerator = numerator;
         this.denominator = denominator;
     }
