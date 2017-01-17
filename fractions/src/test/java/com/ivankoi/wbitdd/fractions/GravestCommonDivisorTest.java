34d733411889a6bd6c94210d5307a603ac59ae81
ivankoi <ivankoi@gmail.com>
2017-01-17 21:42:58 +0100

    Explored GCD algorithm form Wikipedia; found an annoying aspect of its contract

--- fractions/src/test/java/com/ivankoi/wbitdd/fractions/GravestCommonDivisorTest.java
+++ fractions/src/test/java/com/ivankoi/wbitdd/fractions/GravestCommonDivisorTest.java
@@ -8,9 +8,39 @@ import static junit.framework.TestCase.assertEquals;
  * Created by ivank on 1/17/2017.
  */
 public class GravestCommonDivisorTest {
+
     @Test
-    public void oneAndOne() throws Exception {
+    public void reflexive() throws Exception {
         assertEquals(1, gcd(1, 1));
+        assertEquals(2, gcd(2, 2));
+        assertEquals(-1, gcd(-1, -1));
+    }
+
+    @Test
+    public void relativelyPrime() throws Exception {
+        assertEquals(1, gcd(2, 3));
+        assertEquals(1, gcd(4, 7));
+        assertEquals(-1, gcd(-2, -3));
+    }
+
+    @Test
+    public void oneIsTheMultipleOfTheOther() throws Exception {
+        assertEquals(3, gcd(3, 9));
+        assertEquals(5, gcd(5, 30));
+    }
+
+    @Test
+    public void commonFactor() throws Exception {
+        assertEquals(2, gcd(6, 8));
+        assertEquals(7, gcd(49, 315));
+        assertEquals(-4, gcd(-24, -28));
+    }
+
+    @Test
+    public void negatives() throws Exception {
+        // I'm not sure if I like this behaviour
+        assertEquals(4, gcd(-24, 28));
+        assertEquals(-4, gcd(24, -28));
     }
 
     private int gcd(int a, int b) {
