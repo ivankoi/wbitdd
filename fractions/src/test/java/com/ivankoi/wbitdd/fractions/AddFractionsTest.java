ffc74d55606b655820d35d3b40ea1ac67cd62977
ivankoi <ivankoi@gmail.com>
2017-01-17 21:05:42 +0100

    Replaced remaining scaffolding assertions with assertEquals

--- fractions/src/test/java/com/ivankoi/wbitdd/fractions/AddFractionsTest.java
+++ fractions/src/test/java/com/ivankoi/wbitdd/fractions/AddFractionsTest.java
@@ -16,32 +16,31 @@ public class AddFractionsTest {
 
     @Test
     public void zeroPlusZero() throws Exception {
-        assertEquals(0, new Fraction(0).plus(new Fraction(0)).intValue());
+        assertEquals(new Fraction(0), new Fraction(0).plus(new Fraction(0)));
     }
 
     @Test
     public void nonZeroPlusZero() throws Exception {
-        assertEquals(3, new Fraction(3).plus(new Fraction(0)).intValue());
+        assertEquals(new Fraction(3), new Fraction(0).plus(new Fraction(3)));
     }
 
     @Test
     public void zeroPlusNonZero() throws Exception {
-        assertEquals(3, new Fraction(0).plus(new Fraction(3)).intValue());
+        assertEquals(new Fraction(3), new Fraction(3).plus(new Fraction(0)));
     }
 
     @Test
     public void nonNegativeNonZeroOperands() throws Exception {
-        assertEquals(7, new Fraction(3).plus(new Fraction(4)).intValue());
+        assertEquals(new Fraction(7), new Fraction(4).plus(new Fraction(3)));
     }
 
     @Test
     public void negativeInputsAndNegativeOutput() throws Exception {
-        assertEquals(-2, new Fraction(-3).plus(new Fraction(1)).intValue());
+        assertEquals(new Fraction(-2), new Fraction(-3).plus(new Fraction(1)));
     }
 
     @Test
     public void nonTrivialButCommon() throws Exception {
-        Fraction sum = new Fraction(1, 5).plus(new Fraction(2, 5));
-        assertEquals(new Fraction(3, 5), sum);
+        assertEquals(new Fraction(3, 5), new Fraction(1, 5).plus(new Fraction(2, 5)));
     }
 }
