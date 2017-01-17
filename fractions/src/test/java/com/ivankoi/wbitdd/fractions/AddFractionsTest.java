a7a3b0f002153dc110e228013b966eebaef36ad9
ivankoi <ivankoi@gmail.com>
2017-01-17 21:21:26 +0100

    We can add fractions with different denominators as long as we don't have to reduce result

--- fractions/src/test/java/com/ivankoi/wbitdd/fractions/AddFractionsTest.java
+++ fractions/src/test/java/com/ivankoi/wbitdd/fractions/AddFractionsTest.java
@@ -40,7 +40,12 @@ public class AddFractionsTest {
     }
 
     @Test
-    public void nonTrivialButCommon() throws Exception {
+    public void nonTrivialButCommonDenominator() throws Exception {
         assertEquals(new Fraction(3, 5), new Fraction(1, 5).plus(new Fraction(2, 5)));
+    }
+
+    @Test
+    public void differentDenominatorsWithoutReducing() throws Exception {
+        assertEquals(new Fraction(5, 6), new Fraction(1, 2).plus(new Fraction(1, 3)));
     }
 }
